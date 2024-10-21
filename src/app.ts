cae71dc591fa38adaed1a8610c7b519cebe06bfd
brendobrito2002 <brendoplayer@gmail.com>
2024-10-21 14:17:24 -0300

    banco de dados mongodb conectado, dependencias para o banco e o processo de conexao e adicao do arquivo responsavel pela conexao connection

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('Hello World');
+import express from "express";
+
+require('dotenv').config();
+require('./database/connection');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}.`);
+});
