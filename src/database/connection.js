747f3845d14d5af68046fa3479bdc9956880227f
brendobrito2002 <brendoplayer@gmail.com>
2024-10-22 11:53:35 -0300

    banco de dados mongodb configurado

---
+++ src/database/connection.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+
+const connect = () => {
+    mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.6zbxe.mongodb.net/encontrar-lojas-proximas?retryWrites=true&w=majority&appName=Cluster0`)
+        .then(() => {
+            console.log('Conectado ao mongoDB com sucesso.');
+        })
+        .catch((err) => {
+            console.error('Erro ao conectar com o mongoDB:', err);
+        });
+};
+
+connect();
+
+module.exports = mongoose;
